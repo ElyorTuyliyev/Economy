58404df58a85e4e91b353318c6f4fb68665eb59c
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-10-02 08:55:31 +0500

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@mui/material";
+
+type Props = {
+  children: JSX.Element;
+  onclick: () => void;
+  disabled?: boolean;
+  content: string;
+  variant: "text" | "outlined" | "contained";
+};
+
+const CustomButton = ({
+  children,
+  onclick,
+  variant,
+  disabled,
+  content,
+}: Props) => {
+  return (
+    <>
+      <Button variant={variant}></Button>
+      <button disabled={disabled} onClick={onclick}>
+        {content}
+      </button>
+    </>
+  );
+};
+
+export default CustomButton;
+
+CustomButton({
+  content: "Hello",
+  onclick() {},
+  children: <div></div>,
+});
